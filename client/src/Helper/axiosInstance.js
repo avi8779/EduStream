6212727b5fceb3174902a34884e9bd9abffe644f
Avinash kumar <123002279+avi8779@users.noreply.github.com>
2026-05-18 13:24:41 +0530

    Configure axios instance with base URL and headers

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const axiosInstance = axios.create();
-
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, 
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
 export default axiosInstance;
