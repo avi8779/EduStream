304e52605ae4b9ab4dc512dd1eef9b923c2e9c5f
Avinash kumar <123002279+avi8779@users.noreply.github.com>
2026-05-18 15:17:25 +0530

    Remove Content-Type header from axiosInstance
    Removed Content-Type header from axios instance.

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -5,9 +5,6 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true, 
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 export default axiosInstance;
