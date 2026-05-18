228d18bf67e1c9f3897725cbb4726c779d33f2e2
Avinash kumar <123002279+avi8779@users.noreply.github.com>
2026-05-18 12:40:49 +0530

    Use environment variable for base URL in axios instance

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://edustream-3rd7.onrender.com/api/v1";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const axiosInstance = axios.create();
 
